b2c4a96882ae786907a5d2a924fdd077c7cb1bad
vivekmahato26 <vivekmahato26@gmail.com>
2024-03-11 13:14:01 +0530

    init

--- backend/app/config/redis.js
+++ backend/app/config/redis.js
@@ -1,8 +1,10 @@
 let redis = require("redis");
 let client = redis.createClient({
+    // url:  process.env.REDIS_URL,
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT,
-    password: process.env.REDIS_PASS
+    password: process.env.REDIS_PASS,
+    // username: process.env.REDIS_USER
 });
 
 client.on("error", (error) => {
